61bdd472e5133e40a40e80f791e3b1081356a7cf
DaniilMoi <moiseev@179.ru>
2021-10-20 14:22:22 +0300

    Click and Drag on minimap
    Add new script
    Add script to minimap

--- RealTimeStrategy/Assets/Scripts/Networking/RTSPlayer.cs
+++ RealTimeStrategy/Assets/Scripts/Networking/RTSPlayer.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class RTSPlayer : NetworkBehaviour
 {
+    [SerializeField] private Transform cameraTransform = null;
     [SerializeField] private LayerMask buildingBlockLayer = new LayerMask();
     [SerializeField] private Building[] buildings = new Building[0];
     [SerializeField] private float buildingRangeLimit = 5f;
@@ -19,7 +20,15 @@ public class RTSPlayer : NetworkBehaviour
     private List<Unit> myUnits = new List<Unit>();
     private List<Building> myBuildings = new List<Building>();
 
-    public Color GetTeamColor() { return teamColor; }
+    public Transform GetCameraTransform()
+    {
+        return cameraTransform;
+    }
+
+    public Color GetTeamColor()
+    {
+        return teamColor;
+    }
 
     public int GetResources()
     {
